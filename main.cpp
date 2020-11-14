1408f86278f1789f6c33607b2aac2a30c5bec869
Namnyef <71139898+Namnyef@users.noreply.github.com>
2020-11-14 06:30:05 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+
+class BTreeNode
+{
+private:
+    int *keys;
+    int numKeys;
+    int m;
+    bool leaf;
+    BTreeNode **C;
+
+public:
+    BTreeNode(int _t, bool _leaf);
+
+    void traverse();
+
+    BTreeNode *search(int k);
+
+    friend class BTree;
+};
+
+
+class BTree
+{
+private:
+    BTreeNode *root;
+    int m;
+public:
+
+    BTree(int _m)
+    {  root = NULL;  m = _m; }
+
+
+    void traverse()
+    {  if (root != NULL) root->traverse(); }
+
+
+    BTreeNode* search(int key)
+    {  return (root == NULL)? NULL : root->search(key); }
+};
+
+
+BTreeNode::BTreeNode(int _t, bool _leaf)
+{
+
+    m = _m;
+    leaf = _leaf;
+
+    keys = new int[2*m-1];
+    C = new BTreeNode *[2*m];
+
+    numKeys = 0;
+}
